df391bdb6f6b7e1478d8c1387afc81eae7916e4b
AkmalKurbanov <250296a@gmail.com>
2019-09-18 18:36:33 +0600

    dsf

--- firma-help-html/dist/scripts/main.js
+++ firma-help-html/dist/scripts/main.js
@@ -2,11 +2,31 @@
 
 (function ($) {
   $(window).on('load', function () {
-    $('#moadalScroll-js').mCustomScrollbar({
+    $('.modalScroll-js').mCustomScrollbar({
       scrollButtons: {
         enable: true
       }
     });
   });
 })(jQuery);
+
+$('.agree-js').on('click', function () {
+  $('#offer').hide();
+});
+$('.agree-js2').on('click', function () {
+  $('#confidentiality').hide();
+});
+$('.agree-js3').on('click', function () {
+  $('#agree').hide();
+  Cookies.set('modal', 'show', {
+    expires: 7
+  });
+  $('body').removeClass('bodyScroll');
+});
+var modalCookies = Cookies.get('modal');
+
+if (!modalCookies) {
+  $('.modal-agree').show();
+  $('body').addClass('bodyScroll');
+}
 //# sourceMappingURL=main.js.map
