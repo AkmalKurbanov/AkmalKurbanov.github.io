90f75a14d9d18c238470a73a1b893c7dc6ea536d
AkmalKurbanov <250296a@gmail.com>
2019-09-19 11:55:41 +0600

    sd

--- firma-help-html/dist/scripts/main.js
+++ firma-help-html/dist/scripts/main.js
@@ -1,4 +1,4 @@
-"use strict";!function(o){o(window).on("load",function(){o(".modalScroll-js").mCustomScrollbar({scrollButtons:{enable:!0}})})}(jQuery),$(".agree-js").on("click",function(){$("#offer").hide()}),$(".agree-js2").on("click",function(){$("#confidentiality").hide()}),$(".agree-js3").on("click",function(){$("#agree").hide(),$.cookie("modal","show",{expires:182}),$("body").removeClass("bodyScroll")}),"show"!==$.cookie("modal")&&($(".modal-agree").show(),$("body").addClass("bodyScroll"));if ($.cookie("questionBox") !== 'clicked') {
+"use strict";!function(o){o(window).on("load",function(){o(".modalScroll-js").mCustomScrollbar({scrollButtons:{enable:!0}})})}(jQuery),if ($.cookie("questionBox") !== 'clicked') {
     setTimeout(function () {
         $('.questionBox').addClass('askQuestion');
     }, 30000);
