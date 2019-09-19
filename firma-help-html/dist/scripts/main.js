045364c62221fac21874380ef5c0842fef19212d
AkmalKurbanov <250296a@gmail.com>
2019-09-19 12:04:10 +0600

    sd

--- firma-help-html/dist/scripts/main.js
+++ firma-help-html/dist/scripts/main.js
@@ -1,14 +1,1 @@
-"use strict";!function(o){o(window).on("load",function(){o(".modalScroll-js").mCustomScrollbar({scrollButtons:{enable:!0}})})}(jQuery),if ($.cookie("questionBox") !== 'clicked') {
-    setTimeout(function () {
-        $('.questionBox').addClass('askQuestion');
-    }, 30000);
-
-    $('.questionBox__close').on('click', function () {
-        $('.questionBox').removeClass('askQuestion');
-        $.cookie('questionBox', 'clicked', { expires: 300 });
-    });
-    $('.questionBox a').on('click', function () {
-        $.cookie('questionBox', 'clicked', { expires: 300 });
-    });
-}
-
+"use strict";$(window).on("load",function(){$(".modalScroll-js").mCustomScrollbar({scrollButtons:{enable:!0}})}),$(".agree-js").on("click",function(){$("#offer").hide()}),$(".agree-js2").on("click",function(){$("#confidentiality").hide()}),$(".agree-js3").on("click",function(){$("#agree").hide(),$.cookie("modal","show",{expires:182}),$("body").removeClass("bodyScroll")}),"show"!==$.cookie("modal")&&($(".modal-agree").show(),$("body").addClass("bodyScroll"));
