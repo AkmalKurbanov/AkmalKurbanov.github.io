6d8d8f5095bf03ec2cc16650aa65ace5aae48eab
AkmalKurbanov <250296a@gmail.com>
2021-04-22 03:25:07 +0600

    berries

---
+++ berries/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  if (window.screen.width > 1199) {
+    $('.bg-wrapper, .footer-wrapper, .main__berries ul li img, .main__berries ul li span').addClass('layer');
+    var $scene = $('.wrapper, .content, .coins').parallax({
+      scalarX: 10,
+      scalarY: 10,
+    });
+  }
+    
+    $('.main__berries ul').addClass('scale');
+    $('.central').addClass('central-scale');
+    $('header').addClass('slide-top');
+    $('footer').addClass('slide-bottom');
+
+    $('.main__berries ul li').on('click', function () {
+
+      var random = Math.floor(Math.random() * 1000);
+      var $li = $(this).find('span');
+      // var $li = $(".main__berries ul li span");
+      $li.eq(random % $li.length).addClass("--open");
+      $(this).find('.berry').fadeOut();
+      var countClass = $('.main__berries ul li span.--open').length;
+      if (countClass == 3) {
+        $('.main__berries ul li').css('pointer-events', 'none');
+        $('.btn').addClass('get');
+      } else {
+        $('.btn').removeClass('get');
+      }
+
+    });
+
+  });
